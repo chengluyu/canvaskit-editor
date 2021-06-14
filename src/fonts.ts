f24a9dd2ae3453100acd9d33827ce1c80eb676aa
Luyu Cheng <chengluyu@live.cn>
2021-06-14 15:54:55 +0800

    feat(perf): preload fonts before loading CanvasKit

--- src/fonts.ts
+++ src/fonts.ts
@@ -1,15 +1,35 @@
 import { CanvasKit, FontMgr } from "canvaskit-wasm";
 import { fonts } from "../shared/fonts";
 
+const urlArrayBufferMap = new Map<string, ArrayBuffer | Promise<ArrayBuffer>>();
+
 export async function loadArrayBuffer(url: string): Promise<ArrayBuffer> {
-  const response = await fetch(url);
-  const buffer = await response.arrayBuffer();
-  return buffer;
+  const bufferOrPromise = urlArrayBufferMap.get(url);
+  if (bufferOrPromise === undefined) {
+    const promise = fetch(url)
+      .then((response) => response.arrayBuffer())
+      .then((arrayBuffer) => {
+        urlArrayBufferMap.set(url, arrayBuffer);
+        return arrayBuffer;
+      });
+    urlArrayBufferMap.set(url, promise);
+    return promise;
+  } else if (bufferOrPromise instanceof ArrayBuffer) {
+    return Promise.resolve(bufferOrPromise);
+  } else {
+    return bufferOrPromise;
+  }
+}
+
+export function preloadFonts(): void {
+  for (const font of fonts) {
+    loadArrayBuffer(font.runtimeFileName);
+  }
 }
 
 export async function loadFonts(kit: CanvasKit): Promise<FontMgr> {
   const loadedFonts = await Promise.all(
-    fonts.map((x) => loadArrayBuffer(x.runtimeFileName))
+    fonts.map((font) => loadArrayBuffer(font.runtimeFileName))
   );
   const fontMgr = kit.FontMgr.FromData(...loadedFonts);
   if (fontMgr === null) {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import "normalize.css";
 import { CanvasKitInit } from "./canvaskit";
+import { preloadFonts } from "./fonts";
 import { render } from "./render";
 
+preloadFonts();
 CanvasKitInit().then(render);
