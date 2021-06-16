e6a1b6fa3159b004e78f5cb7e535a49f0c18277f
Luyu Cheng <chengluyu@live.cn>
2021-06-17 02:39:24 +0800

    refactor(debug): set cursor to none when drawing cursor

--- src/controls.ts
+++ src/controls.ts
@@ -4,6 +4,13 @@ export let showGlyphBorders = createCheckboxStore("show-glyph-borders");
 
 export let showCursorPosition = createCheckboxStore("show-cursor-position");
 
+showCursorPosition.subscribe((showCursor) => {
+  const canvas = document.querySelector("canvas");
+  if (canvas) {
+    canvas.style.cursor = showCursor ? "none" : "text";
+  }
+});
+
 function createCheckboxStore(selector: string): WritableStore<boolean> {
   const element = document.getElementById(selector) as HTMLInputElement;
   element.addEventListener("change", update);
